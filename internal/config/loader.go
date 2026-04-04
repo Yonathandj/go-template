607f33c3d50b1c7bd6fd0a7bc0be8a3a42dc98f5
yonathandj <yonathandawang111@gmail.com>
2026-04-04 20:30:33 +0700

    refactor: update config & add logger loader

--- internal/config/loader.go
+++ internal/config/loader.go
@@ -1,12 +1,7 @@
 package config
 
 import (
-	"fmt"
-	"strings"
 	"time"
-
-	"github.com/joho/godotenv"
-	"github.com/spf13/viper"
 )
 
 type ServerConfig struct {
@@ -16,7 +11,7 @@ type ServerConfig struct {
 
 type DatabaseConfig struct {
 	Host     string
-	Port     string
+	Port     int
 	Username string
 	Password string
 	Name     string
@@ -26,46 +21,18 @@ type DatabasesConfig struct {
 	Postgre map[string]DatabaseConfig
 }
 
+type LoggerConfig struct {
+	Format string
+	Level  string
+	Output string
+}
+
 type Config struct {
 	Server    ServerConfig
 	Databases DatabasesConfig
+	Logger    LoggerConfig
 }
 
-const (
-	ENV_LOCATION = "../../.env"
-
-	CONFIG_NAME     = "config"
-	CONFIG_TYPE     = "yaml"
-	CONFIG_LOCATION = "../../configs/"
-)
-
 func Load() (*Config, error) {
-	var (
-		config *Config
-		err    error
-	)
-
-	err = godotenv.Load(ENV_LOCATION)
-	if err != nil {
-		return nil, fmt.Errorf("unable to load the .env file: %w", err)
-	}
-
-	viper.SetConfigName(CONFIG_NAME)
-	viper.SetConfigType(CONFIG_TYPE)
-	viper.AddConfigPath(CONFIG_LOCATION)
-
-	err = viper.ReadInConfig()
-	if err != nil {
-		return nil, fmt.Errorf("unable to load the configuration file: %w", err)
-	}
-
-	viper.AutomaticEnv()
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-
-	err = viper.Unmarshal(&config)
-	if err != nil {
-		return nil, fmt.Errorf("unable to unmarshal the config into the struct: %w", err)
-	}
-
-	return config, nil
+	return nil, nil
 }
