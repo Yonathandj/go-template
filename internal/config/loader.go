d9a548cfd5afd92cf426a1c54b440e35a510aa22
yonathandj <yonathandawang111@gmail.com>
2026-04-04 21:13:09 +0700

    refactor: update config loader to handle file and remote configuration

--- internal/config/loader.go
+++ internal/config/loader.go
@@ -1,7 +1,12 @@
 package config
 
 import (
+	"fmt"
+	"strings"
 	"time"
+
+	env "github.com/joho/godotenv"
+	"github.com/spf13/viper"
 )
 
 type ServerConfig struct {
@@ -33,6 +38,51 @@ type Config struct {
 	Logger    LoggerConfig
 }
 
+func loadFile() error {
+	viper.SetConfigName("")
+	viper.SetConfigType("")
+	viper.AddConfigPath("")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return fmt.Errorf("unable to load the configuration file: %w", err)
+	}
+
+	return nil
+}
+
+func loadConsul() error {
+	return nil
+}
+
 func Load() (*Config, error) {
-	return nil, nil
+	var (
+		config      Config
+		environment string
+	)
+
+	err := env.Load("")
+	if err != nil {
+		fmt.Print("unable to load the .env file, relying on system environment variables")
+	}
+
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	environment = viper.GetString("application.environment")
+	switch environment {
+	case "development":
+		loadFile()
+	case "sit", "uat", "production":
+		loadConsul()
+	default:
+		loadFile()
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return nil, fmt.Errorf("unable to unmarshal the config into the struct: %w", err)
+	}
+
+	return &config, nil
 }
